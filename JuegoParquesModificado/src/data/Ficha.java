38cb3e942b270de3009eb668c12dee9740601333
jearizaa <38961983+jearizaa@users.noreply.github.com>
2018-05-15 22:58:13 -0500

    PruebaEscritorio
    Prueba GitHub con NetBeans

--- JuegoParquesModificado/src/data/Ficha.java
+++ JuegoParquesModificado/src/data/Ficha.java
@@ -3,5 +3,51 @@ package data;
 
 
 public class Ficha {
+    private String color;
+    private Casilla posicion;
+    private int avance;
+    private boolean encarcelado;
+    private boolean protegido;
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Casilla getPosicion() {
+        return posicion;
+    }
+
+    public void setPosicion(Casilla posicion) {
+        this.posicion = posicion;
+    }
+
+    public int getAvance() {
+        return avance;
+    }
+
+    public void setAvance(int avance) {
+        this.avance = avance;
+    }
+
+    public boolean isEncarcelado() {
+        return encarcelado;
+    }
+
+    public void setEncarcelado(boolean encarcelado) {
+        this.encarcelado = encarcelado;
+    }
+
+    public boolean isProtegido() {
+        return protegido;
+    }
+
+    public void setProtegido(boolean protegido) {
+        this.protegido = protegido;
+    }
+    
     
 }
