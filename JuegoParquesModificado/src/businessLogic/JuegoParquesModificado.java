c240806d8923d431e98f62c9f085d377d3551fb9
jearizaa <38961983+jearizaa@users.noreply.github.com>
2018-05-16 22:23:24 -0500

    Se ha creado el tablero para el juego
    Se creo un diccionario para establecer la casilla del tablero acorde al avance de la ficha

--- JuegoParquesModificado/src/businessLogic/JuegoParquesModificado.java
+++ JuegoParquesModificado/src/businessLogic/JuegoParquesModificado.java
@@ -1,10 +1,20 @@
 
 package businessLogic;
 
+import data.Tablero;
+
 public class JuegoParquesModificado {
 
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+        
+        Tablero tablero = new Tablero();
+        
+        for(int i = 0; i < tablero.getTablero().length ; i++){
+            System.out.println(Integer.toString(i)+"\t"+ tablero.getTablero()[i]);
+        }
     
+        System.out.println(tablero.mapearCasillas("AMARILLO", 18));
+        
+        
+    }
 }
